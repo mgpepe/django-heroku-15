dcfdc991d6676a5a7a6707265ce722b1f9388bd0
Petar Petrov <petrov.petar@gmail.com>
2014-04-09 17:18:27 +0300

    Revert "first setup"
    This reverts commit 432d594b08df4bb907a115f1b97c6eb7fe12b7cc.

--- portfolio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-		url(r'^$', 'portfolio.views.home', 
-			name = 'psplash'),
-		url(r'^(?P<portfolio_id>\d+)/$', 'portfolio.views.single', name = 'single'),
-)
